88bce5d4408a79f2da797bb229a994ebc3ff96ae
Rayyy990 <arkarphyo99012@gmail.com>
2025-05-14 12:00:04 +0700

    First Commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,21 @@
+package com.db_test;
+
+/**This is the testing Main Class.
+Created by: Arkar
+Created Date: 5/13/2025
+ */
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    /**
+
+     */
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello World!");
+
+
+        }
+    }
